603c318c29a782be8c8b70be937a56e8c9f634f3
agusamblard <142038344+agusamblard@users.noreply.github.com>
2023-10-05 10:21:38 -0300

    Update Nemo.java

--- TP3/nemoproject/Nemo.java
+++ TP3/nemoproject/Nemo.java
@@ -1,32 +1,37 @@
 package nemoproject;
 
 public class Nemo {
+    private int x;
+    private int y;
     private int depth;
-    private int position;
     private String direction;
     private int u = 1;
     private int d = -1;
     private int r = 90;
     private int l = -90;
     private int f = 1;
+    private int[] position;
 
     public Nemo() {
+        this.x = 0;
+        this.y = 0; 
         this.depth = 0;
-        this.position = 0;
         this.direction = "norte";
+        this.position = new int[]{x, y};
     }
 
     public int getDepth() {
-        return this.depth;
+    	return this.depth;
     }
 
-    public int getPosition() {
+    public int[] getPosition() {
         return this.position;
     }
-
+    
     public String getDirection() {
         return this.direction;
     }
+    
     public Nemo move(String command) {
         if (command.equals("u")) {
             this.depth += 1;
@@ -68,7 +73,20 @@ public class Nemo {
         }
 
         if (command.equals("f")) {
-            this.position += 1;
+            switch (this.direction) {
+                case "norte":
+                    this.position[1] += 1;
+                    break;
+                case "sur":
+                    this.position[1] -= 1;
+                    break;
+                case "este":
+                    this.position[0] += 1;
+                    break;
+                case "oeste":
+                    this.position[0] -= 1;
+                    break;
+            }
         }
         return this;
     }
