73c146af7a95789cd52c1b66df1072c7bd8614c5
Julia Pantin <69682517+Juan-2001@users.noreply.github.com>
2023-10-17 19:40:12 -0300

    cambios test

--- TP3/nemoproject/Nemo.java
+++ TP3/nemoproject/Nemo.java
@@ -1,36 +1,49 @@
 package nemoproject;
 
 public class Nemo {
-    private int depth;
-    private int position;
-    private String direction;
-    private int u = 1;
-    private int d = -1;
-    private int r = 90;
-    private int l = -90;
-    private int f = 1;
-
-    public Nemo() {
-        this.depth = 0;
-        this.position = 0;
-        this.direction = "norte";
-    }
-
+	    private int x;
+	    private int y;
+	    private int depth;
+	    private String direction;
+	    private int[] position;
+        /* como procesar los caracteres sin if ni switch, cam handle. dado una coleccion de elementos
+        , para todx elemento en esa coleccion si el elemento cumple que haga algo
+                metodo estatico que se
+                Static command(cha c{
+                {} para todx e   e.cumple(c)
+                 }
+                 tener la idea de un punto en el mapa con las coordenadas x e y variables separadas
+                  na subclase por cada caracter */
+	    public Nemo() {
+	        this.x = 0;
+	        this.y = 0; 
+	        this.depth = 0;
+	        this.direction = "norte";
+	        this.position = new int[]{x, y};
+	    }
     public int getDepth() {
-        return this.depth;
+    	return this.depth;
     }
 
-    public int getPosition() {
+    public int[] getPosition() {
         return this.position;
     }
-
+    
     public String getDirection() {
         return this.direction;
     }
+    
     public Nemo move(String command) {
-        if (command.equals("u")) {
-            this.depth += 1;
-        }
+        command.chars()
+        .mapToObj(c -> (char) c)
+        .forEach(this::processCharacter);
+return this;
+}
+private void processCharacter(char c) {
+String command = String.valueOf(c);
+		if (command.equals("u")) {
+			this.depth += 1;
+		}
         if (command.equals("d")) {
             this.depth -= 1;
         }
@@ -68,8 +81,18 @@ public class Nemo {
         }
 
         if (command.equals("f")) {
-            this.position += 1;
-        }
-        return this;
-    }
-}
+            switch (this.direction) {
+                case "norte":
+                    this.position[1] += 1;
+                    break;
+                case "sur":
+                    this.position[1] -= 1;
+                    break;
+                case "este":
+                    this.position[0] += 1;
+                    break;
+                case "oeste":
+                    this.position[0] -= 1;
+                    break;
+            }
+        }}}
--- TP3/nemoproject/NemoTest.java
+++ TP3/nemoproject/NemoTest.java
@@ -1,39 +1,46 @@
 package nemoproject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class NemoTest {
    @Test public void test00WheresNemo() {
-       // ( profundidad , posicion , rotacion )
-       // rotacion : norte (0) sur(180) este() oeste
-       // public int profundidad;
        assertEquals(0, new Nemo().getDepth());
-       assertEquals(0, new Nemo().getPosition());
+       assertArrayEquals(new int[]{0, 0}, new Nemo().getPosition());
        assertEquals("norte" , new Nemo().getDirection());
-      // esta en la superficie?
-       // en que posicion esta?
-       // para donde esta mirando?
    }
-   @Test public void test01NoMovement() {
+
+  @Test public void test01NoMovement() {
          assertEquals(0, new Nemo().move("").getDepth());
-         assertEquals(0, new Nemo().move("").getPosition());
+         assertArrayEquals(new int[] {0,0}, new Nemo().move("").getPosition());
          assertEquals("norte" , new Nemo().move("").getDirection());
    }
-   @Test public void test02Down() {
+  
+  @Test public void test02MovesDown() {
         assertEquals(-1, new Nemo().move("d").getDepth());
-   }
-   @Test public void test03Up() {
+  }
+  @Test public void test03MovesUp() {
          assertEquals(1, new Nemo().move("u").getDepth());
-   }
-   @Test public void test04Right() {
+  }
+   @Test public void test04RotatesRight() {
          assertEquals("este", new Nemo().move("r").getDirection());
    }
-   @Test public void test05Left() {
+   @Test public void test05RotatesLeft() {
          assertEquals("oeste", new Nemo().move("l").getDirection());
    }
-   @Test public void test06Forward() {
-         assertEquals(1, new Nemo().move("f").getPosition());
+   @Test public void test06MovesForward() {
+         assertArrayEquals(new int[] {0,1}, new Nemo().move("f").getPosition());
+   }
+         
+  @Test public void test07RotationChangesForward() {
+	  assertArrayEquals(new int[] {1,1}, new Nemo().move("frf").getPosition());
+   }
+   @Test public void test08CompoundMovement() {
+	   String command = "ddudflflr";
+	   assertEquals(-2, new Nemo().move(command).getDepth());
+	   assertEquals("oeste", new Nemo().move(command).getDirection());
+	   assertArrayEquals(new int[] {-1,1}, new Nemo().move(command).getPosition());
    }
 }
