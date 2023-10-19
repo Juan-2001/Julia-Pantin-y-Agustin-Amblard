1e591eda9f9bed017aed5a9c50c3938c353f7833
agusamblard <142038344+agusamblard@users.noreply.github.com>
2023-10-19 11:30:44 -0300

    Create Submerged.java

---
+++ TP3/nemoproject/Submerged.java
@@ -0,0 +1,31 @@
+package nemoproject;
+
+public class Submerged extends DepthStatus {
+
+    public int depth;
+    public DepthStatus previousStatus;
+
+    public Submerged(int depth, DepthStatus previousStatus) {
+        this.depth = depth;
+        this.previousStatus = previousStatus;
+    }
+
+    public int getDepth() {
+        return this.depth;
+    }
+
+    public DepthStatus ascend() {
+        return this.previousStatus;
+    }
+
+    public DepthStatus descend() {
+    	this.previousStatus = new Submerged(this.depth , this.previousStatus);
+    	this.depth -=1;
+    	return new Submerged(this.depth, this.previousStatus);
+    	
+    }
+
+    public void releaseCapsule() {
+        throw new RuntimeException("El submarino explota.");
+    }
+}
