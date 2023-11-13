f328fa0cc0f70393925b23a712ad0d1f581ab1a6
Agustin Amblard <aamblard@udesa.edu.ar>
2023-11-13 01:55:57 -0300

    Update GameOver.java

--- TP4:cuatro en linea/src/linea/GameOver.java
+++ TP4:cuatro en linea/src/linea/GameOver.java
@@ -1,14 +1,27 @@
 package linea;
 
 public class GameOver extends EstadoJuego {
-    public GameOver(Linea juego) {
-        super(juego);
+    public String GAME_OVER = "Game Over!";
+    private final String winner;
+    public GameOver(char winner) {
+        this.winner = winner == Linea.redPlayer ? "Red" : "Blue";
     }
-    public void handle(int columna) {
-        // No hacer nada, termino.
+    public EstadoJuego playRedAs(int desiredColumn, Linea game) {
+        throw new RuntimeException(GAME_OVER);
     }
-    public String jugadorActual() {
-        return "Game Over";
+    public EstadoJuego playBlueAs(int desiredColumn, Linea game) {
+        throw new RuntimeException(GAME_OVER);
     }
-
+    public String getStatus() {
+        return "Game Over! \nWinner: " + this.winner;
+    }
+    public boolean isFinished() {
+        return true;
+    }
+    public boolean equals(Object other) {
+        if (other.getClass() == GameOver.class) {
+            return this.getStatus().equals(((GameOver) other).getStatus());
+        }
+        return false;
+    }
 }
